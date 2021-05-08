58977f77b1f35254f03e14ea4d0a8177178463e5
Yashshree18 <83826959+Yashshree18@users.noreply.github.com>
2021-05-08 20:45:14 +0530

    Update area.py
    This is area.py file

--- area.py
+++ area.py
@@ -2,3 +2,10 @@ pi = 3.14
 r = float(input('Enter the value of radius:'))
 a = pi*r*r
 print('The area of circle=',a)
+
+
+
+
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
